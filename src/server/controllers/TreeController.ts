fed340eb7236a772a872a91a0f4fe7e26f022469
Carter Himmel <carter@elhnet.net>
2019-12-02 12:13:47 -0700

    fix: potential fix?

--- src/server/controllers/TreeController.ts
+++ src/server/controllers/TreeController.ts
@@ -62,7 +62,7 @@ export default class TeamTreesController extends BaseController {
 
     private _parseUser(user: string, i: number, top = false): User {
         const name = user.match(/<strong.*?>(.*?)<\/strong>/m)![1];
-        const amount = user.match(/<span.*?class="(feed-tree-count.*)">(.*) tree.*<\/span>/m)![2];
+        const amount = user.match(/<span.*?class="(feed-tree-count.*)">(.*) tree.*<\/span>/m)![1];
         const message = user.match(/<span.*?class="((?!feed-datetime|feed-tree-count).)*">(.*?)<\/span>/m)![2];
         const date = user.match(/<span.*?>(.*(\d+:\d+:\d+).*)<\/span>/m)![1];
         const image = user.match(/<img.*?src="(.*?)">/m)![1];
