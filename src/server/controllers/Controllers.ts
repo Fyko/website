8cb5d91bddef7a9dbd33375f20119c078fd9a5bb
Carter Himmel <carter@elhnet.net>
2019-12-23 21:50:06 -0700

    fix: export tools in main controllers

--- src/server/controllers/Controllers.ts
+++ src/server/controllers/Controllers.ts
@@ -1,7 +1,15 @@
 import BotController from './BotController';
 import RootController from './HomeController';
+import ToolsController from './ToolsController';
 import TranscriptController from './TranscriptController';
 import TeamTreesController from './TreeController';
 import URLController from './URLController';
 
-export default [BotController, TranscriptController, TeamTreesController, URLController, RootController];
+export default [
+	BotController,
+	ToolsController,
+	TranscriptController,
+	TeamTreesController,
+	URLController,
+	RootController,
+];
